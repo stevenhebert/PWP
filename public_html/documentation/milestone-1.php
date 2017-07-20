7104d406dd616bb7bbde1495030625f2f7f94ce6
steven hebert <shebert2@cnm.edu>
2017-07-20 08:11:24 -0600

    committing first draft of my pwp

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+
+		<meta charset="UTF-8">
+
+		<title>stevens personal website project</title>
+
+	</head>
+
+	<body>
+
+		<div>purpose: I would like to personal website to showcase my web development skills and professional interests.</div>
+
+
+		<div>audience: This webpage is designed for my professional peer group, both to collaborate with other developers and
+			as a presence for my past works.</div>
+
+
+		<div>goal: I maintain this page with the intention of building a clientele base.</div>
+
+
+		<div>persona:</div>
+
+		<ul>
+
+			<li>NAME) Gene</li>
+			<li>DESCRIPTION) Gene is a chief technology officer for a tech startup looking to hire budding junior
+				developers</li>
+			<li>ROLE) As a CTO, Gene is familiar with all aspects of a companies technological support.</li>
+			<li>BEHAVIORS/ATTITUDES) His previous experience with both front end and back end has lead him to his current
+				position. Often frustrated by then lack of coordination between these divisions, he's looking to hire developers
+				that also understand this barrier. He feels this is especially important for startups, where synergy is vital
+				to their survival.</li>
+			<li>GOAL)</li>
+			<li>ADD INFO)</li>
+
+		</ul>
+
+
+	</body>
+</html>
