31c1c3112e5776b8992a1214ee8c050f8b1bfa25
steven <shebert2@cnm.edu>
2017-07-24 02:38:08 -0600

    added use case

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -31,19 +31,24 @@
 				developer to help with backend operations. Gene, a senior developer, comes to XYZ from established firm. His expertise is
 				needed to establish a minimal problem engineering team that can effectively produce and revise mobile applications.</li>
 			<li><strong>Goals </strong>Gene is seeking an employee that understands his/her role in XYZ. Gene's employee will be joining
-				one or two other backend developers so he needs someone with proven experience. Under a lot of pressure already, Gene
-				has little time to parse resumes and headhunt, he's looking for a concise summary of a potential employees experience and
-				interests.</li>
+				one or two frontend developers, his ideal applicant possesses knowledge of both spectrum. Under a lot of pressure already,
+				Gene has little time to parse resumes and headhunt, he's looking for a concise summary of a potential employees experience
+				and interests.</li>
 			<li><strong>Technology </strong>XYZ maintains an up-to-date toolbox of phones, tablets, and computers used to test their software.
 				iOS and Android devices are most common following with macOS and Windows10, occasionally Gene may test with a linux or
 				windows mobile device. XYZ employs Google Fiber for their broadband connection and Verizon for their mobile connection.</li>
-			<li><strong>Frustrations </strong>His previous experience with both front end and back end has lead him to his current
-				position. Often frustrated by then lack of coordination between these divisions, he's looking to hire developers
-				that also understand this barrier. He feels this is especially important for startups, where synergy is vital
-				to their survival.</li>
 			<li><strong>additional information </strong></li>
 
 		</ul>
+
+		<h1>Use Case</h1>
+			<p>It has been a week since Gene posted XYZ's employment opportunity. His inbox has blown up with a vareity of individuals interested
+			in the position. Gene downloads the attachments and eliminates those missing certain keywords. Next he searches the remaining resumes
+			for familiar references and project names.</p>
+
+			<p>He strips the contact information from the remaining resumes and imports them to a contact list. Gene then bcc's the list a request
+			for a project with experience relevant to the position or a portfolio of the applicants past work. After a couple days Gene will pick
+				ten finalists for an interview and offer one of them the position. </p>
 
 	</body>
 </html>
