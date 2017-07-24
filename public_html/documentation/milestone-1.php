23699850c484d4f24613e0edf2c154bd5c6b0039
steven <shebert2@cnm.edu>
2017-07-24 02:39:10 -0600

    improved some formatting

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -27,16 +27,20 @@
 
 		<ul>
 			<li><strong>Name </strong>Gene Blue</li>
+
 			<li><strong>Description </strong>Gene is the chief technology officer at XYZ, a tech startup. XYZ is searching for a junior
 				developer to help with backend operations. Gene, a senior developer, comes to XYZ from established firm. His expertise is
 				needed to establish a minimal problem engineering team that can effectively produce and revise mobile applications.</li>
+
 			<li><strong>Goals </strong>Gene is seeking an employee that understands his/her role in XYZ. Gene's employee will be joining
 				one or two frontend developers, his ideal applicant possesses knowledge of both spectrum. Under a lot of pressure already,
 				Gene has little time to parse resumes and headhunt, he's looking for a concise summary of a potential employees experience
 				and interests.</li>
+
 			<li><strong>Technology </strong>XYZ maintains an up-to-date toolbox of phones, tablets, and computers used to test their software.
 				iOS and Android devices are most common following with macOS and Windows10, occasionally Gene may test with a linux or
 				windows mobile device. XYZ employs Google Fiber for their broadband connection and Verizon for their mobile connection.</li>
+
 			<li><strong>additional information </strong></li>
 
 		</ul>
