65f2bf83c110ba85345f3e0778354ae66120ec49
steven <shebert2@cnm.edu>
2017-07-24 02:16:40 -0600

    changed persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -30,10 +30,13 @@
 			<li><strong>Description </strong>Gene is the chief technology officer at XYZ, a tech startup. XYZ is searching for a junior
 				developer to help with backend operations. Gene, a senior developer, comes to XYZ from established firm. His expertise is
 				needed to establish a minimal problem engineering team that can effectively produce and revise mobile applications.</li>
-			<li><strong>Goals </strong>As a CTO, Gene is familiar with all aspects of a companies technological support.</li>
+			<li><strong>Goals </strong>Gene is seeking an employee that understands his/her role in XYZ. Gene's employee will be joining
+				one or two other backend developers so he needs someone with proven experience. Under a lot of pressure already, Gene
+				has little time to parse resumes and headhunt, he's looking for a concise summary of a potential employees experience and
+				interests.</li>
 			<li><strong>Technology </strong>XYZ maintains an up-to-date toolbox of phones, tablets, and computers used to test their software.
 				iOS and Android devices are most common following with macOS and Windows10, occasionally Gene may test with a linux or
-				windows mobile device.</li>
+				windows mobile device. XYZ employs Google Fiber for their broadband connection and Verizon for their mobile connection.</li>
 			<li><strong>Frustrations </strong>His previous experience with both front end and back end has lead him to his current
 				position. Often frustrated by then lack of coordination between these divisions, he's looking to hire developers
 				that also understand this barrier. He feels this is especially important for startups, where synergy is vital
