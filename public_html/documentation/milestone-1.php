81f2e1e1ee58b111ed3f46e6c74fb8f93cccb5b6
steven <shebert2@cnm.edu>
2017-07-24 02:05:30 -0600

    fixed formatting errors and directory files, added README

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -2,33 +2,43 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>stevens personal website project</title>
+		<title>(MS1) Personal Web Project by Steven Hebert</title>
 
 	</head>
 
 	<body>
-		<h1>Purpose, Audience, Goal</h1>
+		<h1>Purpose</h1>
+			<p>This webpage serves to showcase my software development skills within my professional interests.</p>
 
-		<ol>
-			<li>purpose: I would like to personal website to showcase my web development skills and professional interests.</li>
-			<li>audience: This webpage is designed for my professional peer group, both to collaborate with other developers and
-				as a presence for my past works.</li>
-			<li>goal(s): I maintain this page with the intention of building a clientele base</li>
+		<h1>Audience</h1>
+			<ul>
+				<li>Clients</li>
+				<li>Parters</li>
+				<li>Academic peers</li>
+				<li>Family and friends</li>
 
-		</ol>
+			</ul>
+
+		<h1>Goals</h1>
+			<p>Establish a professional web presence, acquire internships or part-time work, and summarize my past and current projects.</p>
+
 
 		<h1>Persona</h1>
 
 		<ul>
-			<li>name: Gene</li>
-			<li>description: Gene is a chief technology officer for a tech startup looking to hire budding junior
-				developers</li>
-			<li>goals: As a CTO, Gene is familiar with all aspects of a companies technological support.</li>
-			<li>frustrations: His previous experience with both front end and back end has lead him to his current
+			<li><strong>Name </strong>Gene Blue</li>
+			<li><strong>Description </strong>Gene is the chief technology officer at XYZ, a tech startup. XYZ is searching for a junior
+				developer to help with backend operations. Gene, a senior developer, comes to XYZ from established firm. His expertise is
+				needed to establish a minimal problem engineering team that can effectively produce and revise mobile applications.</li>
+			<li><strong>Goals </strong>As a CTO, Gene is familiar with all aspects of a companies technological support.</li>
+			<li><strong>Technology </strong>XYZ maintains an up-to-date toolbox of phones, tablets, and computers used to test their software.
+				iOS and Android devices are most common following with macOS and Windows10, occasionally Gene may test with a linux or
+				windows mobile device.</li>
+			<li><strong>Frustrations </strong>His previous experience with both front end and back end has lead him to his current
 				position. Often frustrated by then lack of coordination between these divisions, he's looking to hire developers
 				that also understand this barrier. He feels this is especially important for startups, where synergy is vital
 				to their survival.</li>
-			<li>additional information:</li>
+			<li><strong>additional information </strong></li>
 
 		</ul>
 
