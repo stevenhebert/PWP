1e67650860c8229b34191c8bace87b42d0f62bd8
steven <shebert2@cnm.edu>
2017-07-25 11:56:52 -0600

    added doc blocks for milestone-2a

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * personal webpage project by Steven
+ * milestone 2 alpha
+ *
+ * here I outline the kind of content I plan to feature on my site and document its intended purpose
+ * //brief, succinct, < 1 paragraph, style-less
+ *
+ * a wireframe visual accompanies the documentation
+ * //wireframes must be, 2mb, .svg | .jpeg | .png | .gif
+ */
+
