391c9af8076a5edaf9280d2b474a221e22dfc8b4
steven <shebert2@cnm.edu>
2017-08-02 01:34:47 -0600

    added content strategy for MS2

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -11,14 +11,20 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 	<head>
+
 		<meta charset="UTF-8">
 		<title>(MS2) Personal Web Project by Steven Hebert</title>
+
 	</head>
 
 	<body>
+
 		<h1>Content Strategy</h1>
+
 		<ul>
+
 			<li>Header: My websites header will contain anchors directing viewers to different content locations on the
 				page. My preliminary idea for
 				content-sections include, "Home", "Code", "Contact", and "Resume".
@@ -40,60 +46,14 @@
 			</li>
 			<li>Resume: Here an integrated? (not downloadable) resume would be presented. This, in addition to my coding
 				experience would list my other professional credentials.
+				<li>A nice solid color footing contrasted to the pages background such that it that gently interrupts the
+				users downward scroll, visually informing them that although their viewing is welcomed, the presentation of
+				additional content is no more. To lessen this blow, a mini-sitemap might also make an appearance anchoring
+				the user to the main-landing and the contact section.
 			</li>
-		</ul>
-
-		<h1>Goals</h1>
-		<p>Establish a professional web presence, acquire internships or part-time work, and summarize my past and current
-			projects.</p>
-
-
-		<h1>Persona</h1>
-
-		<ul>
-			<li><strong>Name </strong>Gene Blue</li>
-
-			<li><strong>Description </strong>Gene is the chief technology officer at XYZ, a tech startup. XYZ is searching
-				for a junior
-				developer to help with backend operations. Gene, a senior developer, comes to XYZ from established firm. His
-				expertise is
-				needed to establish a minimal problem engineering team that can effectively produce and revise mobile
-				applications.
-			</li>
-
-			<li><strong>Goals </strong>Gene is seeking an employee that understands his/her role in XYZ. Gene's employee
-				will be joining
-				one or two frontend developers, his ideal applicant possesses knowledge of both spectrum. Under a lot of
-				pressure already,
-				Gene has little time to parse resumes and headhunt, he's looking for a concise summary of a potential
-				employees experience
-				and interests.
-			</li>
-
-			<li><strong>Technology </strong>XYZ maintains an up-to-date toolbox of phones, tablets, and computers used to
-				test their software.
-				iOS and Android devices are most common following with macOS and Windows10, occasionally Gene may test with
-				a linux or
-				windows mobile device. XYZ employs Google Fiber for their broadband connection and Verizon for their mobile
-				connection.
-			</li>
-
-			<li><strong>Additional Information </strong></li>
 
 		</ul>
-
-		<h1>Use Case</h1>
-		<p>It has been a week since Gene posted XYZ's employment opportunity. His inbox has blown up with a vareity of
-			individuals interested
-			in the position. Gene downloads the attachments and eliminates those missing certain keywords. Next he searches
-			the remaining resumes
-			for familiar references and project names.</p>
-
-		<p>He strips the contact information from the remaining resumes and imports them to a contact list. Gene then
-			bcc's the list a request
-			for a project with experience relevant to the position or a portfolio of the applicants past work. After a
-			couple days Gene will pick
-			ten finalists for an interview and offer one of them the position. </p>
 
 	</body>
+
 </html>
