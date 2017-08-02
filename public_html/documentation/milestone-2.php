a8359fe7b4738ceddd936759c439b2f5c201b89c
steven <shebert2@cnm.edu>
2017-08-02 01:46:25 -0600

    added content strategy for MS2

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -25,31 +25,28 @@
 
 		<ul>
 
-			<li>Header: My websites header will contain anchors directing viewers to different content locations on the
-				page. My preliminary idea for
-				content-sections include, "Home", "Code", "Contact", and "Resume".
+			<li>Header: My website header will contain anchors directing viewers to different content locations on the
+				page. My preliminary ideas for content-sections include, "Home", "Code", "Contact", and "Resume".
 			</li>
-			<li>Main: This will be the main-landing for those who visit my page. I will provide a brief description of the
-				websites content and myself; perhaps a
-				sentence providing context for the content that follows, and some words or a quote that I might use to
-				describe myself.
+			<li>Main: This will be the main-landing for my website visitors. I will provide a brief description of the
+				pages content and myself; perhaps a sentence providing context for the content that follows, and some words
+				or a quote that I might use to describe myself.
 			</li>
 			<li>Code: This section will present my greatest coding-accomplishments and current coding-projects. It MIGHT
 				even be formatted as a visual-resume of sort where I can map where I've been, where I am, and where I'm
-				thinking about going (in terms of code). Depending the type of project I will include a link to its
-				repository or a live-link to the project itself.
+				thinking about going (in a code skill sense). Depending on the type of project, I would include either a
+				link to its repository or a live-link to the project itself.
 			</li>
-			<li>Contact: This will bring the website patron to a section where they can contact me. Initial thoughts for
-				modes of contact include: email, work phone, and/or slackId. Additionally, there will be a form where the
-				user
-				can send me a text-only-email-message/inquiry directly from the site.
+			<li>Contact: Here my websites patron will be presented several acceptable ways to communicate with me. Initial
+				thoughts for acceptable modes of contact include: email, work phone, and/or slackId. Additionally, there
+				will be a form where the user can send me a text-only-email-message/inquiry directly from the site.
 			</li>
-			<li>Resume: Here an integrated? (not downloadable) resume would be presented. This, in addition to my coding
-				experience would list my other professional credentials.
-				<li>A nice solid color footing contrasted to the pages background such that it that gently interrupts the
-				users downward scroll, visually informing them that although their viewing is welcomed, the presentation of
-				additional content is no more. To lessen this blow, a mini-sitemap might also make an appearance anchoring
-				the user to the main-landing and the contact section.
+			<li>Resume: Here an integrated? (non-downloadable) resume would be presented. This, in addition to my coding
+				experience would list my other professional experience, skills, and credentials.
+				<li>A nice solid color footing contrasted against the pages background such that it that gently interrupts
+				the users downward scroll, visually informing them that although their viewing is welcomed, the presentation of
+				additional content is no more. To lessen this blow, a "top" anchor makes an appearance anchoring the user
+				back to the pages main-landing.
 			</li>
 
 		</ul>
