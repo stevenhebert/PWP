a2b9e6ee76bf6067e5f3e9e514147dfd6691d0fd
steven <shebert2@cnm.edu>
2017-08-31 11:20:22 -0600

    added gmail email to contact form recipients

---
+++ public_html/resume.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- from BS4 docs -->
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"
+				integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+		<!-- jQuery -->
+		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"
+				  integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n"
+				  crossorigin="anonymous"></script>
+		<!-- Tether -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"
+				  integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb"
+				  crossorigin="anonymous"></script>
+		<!-- Bootstrap JS -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"
+				  integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn"
+				  crossorigin="anonymous"></script>
+
+
+		<!-- from BCC form -->
+		<!-- jQuery Form, Additional Methods, Validate -->
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+		<!-- Your JavaScript Form Validator -->
+		<script src="js/form-validate.js"></script>
+		<!-- Google reCAPTCHA -->
+		<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
+
+		<!-- Google Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Playball|Roboto:400,400i,700,700i" rel="stylesheet"/>
+
+		<!-- Font Awesome -->
+		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+				rel="stylesheet">
+
+		<!-- custom stylesheet -->
+		<link rel="stylesheet" href="css/resume.css"/>
+
+		<!-- HTML5 shim and Respond . js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond . js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<title>steven hebert</title>
+
+	</head>
+
+	<body>
+		<div class="container">
+
+			<div id="resume" class="yui-t7">
+				<div id="inner">
+
+					<div id="hd">
+						<div class="yui-gc">
+							<div class="yui-u first">
+								<h1>Steven Hebert</h1>
+								<h2>Web Developer, Social Scientist</h2>
+							</div>
+
+							<div class="yui-u">
+								<div class="contact-info">
+									<h3><a id="pdf" href="docs/steven-hebert-resume.pdf">Download PDF</a></h3>
+									<h3><a href="mailto:hebertsteven@me.com">hebertsteven@me.com</a></h3>
+									<h3>(501) 590-7814</h3>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div id="bd">
+						<div id="yui-main">
+							<div class="yui-b">
+
+								<div class="yui-gf">
+									<div class="yui-u first">
+										<h2>Profile</h2>
+									</div>
+									<div class="yui-u">
+										<p class="enlarge">
+											A self-starting, highly-motivated analytical thinker interested in the nexus of energy,
+											technology and the environment.
+										</p>
+									</div>
+								</div>
+
+								<div class="yui-gf">
+									<div class="yui-u first">
+										<h2>Skills</h2>
+									</div>
+									<div class="yui-u">
+
+										<div class="talent">
+											<h2>Web Development</h2>
+											<p>Assertively exploit wireless initiatives rather than synergistic core
+												competencies. </p>
+										</div>
+
+										<div class="talent">
+											<h2>Data Analytics</h2>
+											<p>Credibly streamline mission-critical value with multifunctional
+												functionalities. </p>
+										</div>
+
+										<div class="talent">
+											<h2>Research</h2>
+											<p>Proven ability to lead and manage a wide variety of design and development projects
+												in team and independent situations.</p>
+										</div>
+									</div>
+								</div>
+
+								<div class="yui-gf">
+									<div class="yui-u first">
+										<h2>Technical</h2>
+									</div>
+									<div class="yui-u">
+										<ul class="talent">
+											<li>XHTML</li>
+											<li>CSS</li>
+											<li class="last">Javascript</li>
+										</ul>
+
+										<ul class="talent">
+											<li>Jquery</li>
+											<li>PHP</li>
+											<li class="last">Git / Github</li>
+										</ul>
+
+										<ul class="talent">
+											<li>Stata</li>
+											<li>R</li>
+											<li class="last">MatLab/li>
+										</ul>
+									</div>
+								</div>
+
+								<div class="yui-gf">
+
+									<div class="yui-u first">
+										<h2>Experience</h2>
+									</div>
+
+									<div class="yui-u">
+
+										<div class="job">
+											<h2>University of New Mexico</h2>
+											<h3>Graduate Assistant</h3>
+											<h4>2015-2017</h4>
+											<p>Created surveys, gathered data, created datasets. Developed hypothesis’, reviewed
+												and designed analytical models. Programmed regressions and dynamic simulations.
+												Assessed statistical significance, interpreted and translated results.</p>
+										</div>
+
+										<div class="job">
+											<h2>Arkansas Department of Health</h2>
+											<h3>Specialist III</h3>
+											<h4>2014-2015</h4>
+											<p>Collected, cleaned, and databased clinic outbreak data. Analyzed health and center
+												monthly financial data, created forecasts and visualizations. Recommended and
+												developed budget and policy changes.</p>
+										</div>
+
+										<div class="job last">
+											<h2>Hosto Buchan PLLC</h2>
+											<h3>Portfolio Analyst</h3>
+											<h4>2013-2014</h4>
+											<p>Managed legal portfolios, analyzed performance reports and forecasted earnings.
+												Created database queries and spreadsheet macros to flag statute and discovery data.
+												Verified account data, reviewed and recommended for litigation.
+											</p>
+										</div>
+
+									</div>
+								</div>
+
+
+								<div class="yui-gf last">
+									<div class="yui-u first">
+										<h2>Education</h2>
+									</div>
+									<div class="yui-u">
+										<h2>University of New Mexico - Albuquerque, New Mexico</h2>
+										<h3>Master of Arts, Applied Microeconomics</h3>
+									</div>
+
+									<div class="yui-u">
+										<h2>University of Arkansas - Little Rock, Arkansas</h2>
+										<h3>Mathematics and Statistics</h3>
+									</div>
+
+									<h2>Arkansas State University - Little Rock, Arkansas</h2>
+									<h3>Bachelors of Science, Economics</h3>
+
+									<h2>Jiangxi University of Finance and Economics - Jiangxi, Nanchang, PRC</h2>
+									<h3>Study Abroad</h3>
+
+
+								</div>
+
+
+							</div>
+						</div>
+					</div>
+
+				</div>
+
+
+			</div>
+
+		</div>
+	</body>
+</html>
