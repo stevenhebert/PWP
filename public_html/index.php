64702aa95578e4a20d51ed4f535cf450f6221b06
steven <shebert2@cnm.edu>
2017-09-06 04:31:46 -0600

    formatting added resume no time for latex

--- public_html/index.php
+++ public_html/index.php
@@ -22,9 +22,12 @@
 
 
 		<!-- jQuery Form, Additional Methods, Validate, Google reCAPTCHA -->
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
 		<script src="js/form-validate.js"></script>
 		<script src="https://www.google.com/recaptcha/api.js" async defer></script>
 		<script src="js/recaptcha.js"></script>
@@ -39,7 +42,6 @@
 		<script type="text/javascript" src="js/onepagescroll.js"></script>
 
 		<!-- My CSS -->
-		<link href="css/onepage-scroll.css" rel="stylesheet" type="text/css">
 		<link href="css/style.css" rel="stylesheet" type="text/css">
 
 		<!-- HTML5 shim and Respond . js for IE8 support of HTML5 elements and media queries -->
@@ -68,62 +70,74 @@
 
 				<section id="portfolio" class="page2">
 					<div class="container">
-
 						<h1>portfolio</h1>
 
-						<div class="card-columns">
+						<div class="card-deck">
 							<div class="card">
 								<img class="card-img-top img-fluid" src="images/card1.png" alt="Albuquerque Town Hall">
 								<div class="card-block">
-									<p class="card-text">My Deep Dive capstone project. Created to help engage residents in local government. Built with three of my cohort members in ten short weeks on a LAMP-stack platform. Features <a class="ABQ Open" href="https://www.cabq.gov/abq-data/">ABQ Open</a> GIS data to help educate and inform city residents. The prototype was virtual Town Hall at the city council district level. However, we designed made the platform flexible enough to such that it can scale to model a Town Hall at any level of government.</p>
+									<p class="card-text">My Deep Dive capstone project. Created to help engage residents in
+										local government. Built with three of my cohort members in ten short weeks on a
+										LAMP-stack platform. Features <a class="ABQ Open"
+																					href="https://www.cabq.gov/abq-data/">ABQ Open</a> GIS
+										data to help educate and inform city residents. The prototype was virtual Town Hall at
+										the city council district level. However, we designed made the platform flexible enough
+										to such that it can scale to model a Town Hall at any level of government.</p>
 								</div>
 							</div>
 
 							<div class="card">
 								<img class="card-img-top img-fluid" src="images/card2.png" alt="Synthetic Control Model">
 								<div class="card-block">
-									<p class="card-text">This is a plot from a research paper I completed in graduate school, it uses a modern regression analysis technique called the Synthetic Control Model. In contrast to traditional regression analysis, SCM allows uses co-founding factors to explain account for changes in the dependent and independent variables, where as these might be taken as unobserved or omitted variables in a traditional analysis.</p>
+									<p class="card-text">This is a plot from a research paper I completed in graduate school,
+										it uses a modern regression analysis technique called the Synthetic Control Model. In
+										contrast to traditional regression analysis, SCM allows uses co-founding factors to
+										explain account for changes in the dependent and independent variables, where as these
+										might be taken as unobserved or omitted variables in a traditional analysis.</p>
+								</div>
+							</div>
+						</div>
+
+						<div class="card-deck">
+							<div class="card">
+								<img class="card-img-top img-fluid" src="images/card1.png" alt="Albuquerque Town Hall">
+								<div class="card-block">
+									<p class="card-text">My Deep Dive capstone project. Created to help engage residents in
+										local government. Built with three of my cohort members in ten short weeks on a
+										LAMP-stack platform. Features <a class="ABQ Open"
+																					href="https://www.cabq.gov/abq-data/">ABQ Open</a> GIS
+										data to help educate and inform city residents. The prototype was virtual Town Hall at
+										the city council district level. However, we designed made the platform flexible enough
+										to such that it can scale to model a Town Hall at any level of government.</p>
 								</div>
 							</div>
 
 							<div class="card">
-								<img class="card-img img-fluid" src="..." alt="Card image">
-							</div>
-
-							<div class="card p-3 text-justify">
-								<blockquote class="card-blockquote">
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-									<footer>
-										<small class="text-muted">
-											Someone famous in <cite title="Source Title">Source Title</cite>
-										</small>
-									</footer>
-								</blockquote>
-							</div>
-
-							<div class="card">
+								<img class="card-img-top img-fluid" src="images/card2.png" alt="Synthetic Control Model">
 								<div class="card-block">
-									<h4 class="card-title">Card title</h4>
-									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-										additional content. This card has even longer content than the first to show that equal
-										height action.</p>
-									<p class="card-text">
-										<small class="text-muted">Last updated 3 mins ago</small>
-									</p>
+									<p class="card-text">This is a plot from a research paper I completed in graduate school,
+										it uses a modern regression analysis technique called the Synthetic Control Model. In
+										contrast to traditional regression analysis, SCM allows uses co-founding factors to
+										explain account for changes in the dependent and independent variables, where as these
+										might be taken as unobserved or omitted variables in a traditional analysis.</p>
 								</div>
 							</div>
 						</div>
 
 					</div>
+
 				</section>
+
 
 				<section id="contact" class="page3">
 					<div class="container">
+						<h1>contact</h1>
+
 						<form id="contact-form" action="php/mailer.php" method="POST">
 
 							<div class="form-group row">
 								<label for="name" class="col-xs-1 col-form-label"></label>
-								<div class="input-group col-sm">
+								<div class="input-group col-xs">
 									<div class="input-group-addon">
 										<i class="fa fa-user fa-fw" aria-hidden="true"></i>
 									</div>
@@ -133,7 +147,7 @@
 
 							<div class="form-group row">
 								<label for="email" class="col-xs-1 col-form-label"></label>
-								<div class="input-group col-sm">
+								<div class="input-group col-xs">
 									<div class="input-group-addon">
 										<i class="fa fa-envelope fa-fw" aria-hidden="true"></i>
 									</div>
@@ -143,7 +157,7 @@
 
 							<div class="form-group row">
 								<label for="subject" class="col-xs-1 col-form-label"></label>
-								<div class="input-group col-sm">
+								<div class="input-group col-xs">
 									<div class="input-group-addon">
 										<i class="fa fa-pencil fa-fw" aria-hidden="true"></i>
 									</div>
@@ -152,26 +166,27 @@
 							</div>
 
 							<div class="form-group row">
-								<div class="input-group col-sm-11">
+								<div class="input-group col-xs-11">
 									<div class="input-group-addon">
 										<i class="fa fa-comment fa-fw" aria-hidden="true"></i>
 									</div>
-									<textarea class="form-control " rows="4" id="message" name="message" placeholder="message"></textarea>
+									<textarea class="form-control " rows="4" id="message" name="message"
+												 placeholder="message"></textarea>
 								</div>
 
-								<div class="btn-group-vertical col-sm-1" role="group">
-									<button class="btn g-recaptcha" data-sitekey="6LfiyS0UAAAAAINUJI0YG4Z87VVqs_QdbaRg7EnS" data-callback='onSubmit'>Submit</button>
-									<button class="btn" type="reset"> Reset</button>
+								<div class="col-xs-1">
+									<button class="btn btn-secondary g-recaptcha"
+											  data-sitekey="6LfiyS0UAAAAAINUJI0YG4Z87VVqs_QdbaRg7EnS"
+											  data-callback='onSubmit'>Submit
+									</button>
+									<button class="btn btn-secondary " type="reset"> Reset</button>
 								</div>
 							</div>
 
 							<div class="row">
-								<div class="col-sm">
 									<div id="output-area"></div>
-								</div>
 							</div>
 						</form>
-
 
 					</div>
 
